693b707658af4b154b01d48a1d4d878ed5e16b34
test@gmail.com <test@gmail.com>
2022-02-20 23:34:28 +0000

    test commit

--- src/main/java/com/soj/polymorphism/Main.java
+++ src/main/java/com/soj/polymorphism/Main.java
@@ -10,18 +10,22 @@ public class Main {
     static final Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        logger.debug("Compiletime Polymorphism");
 Shapes shapes=new Shapes();
 logger.debug("Area of Circle : {}",shapes.area(2));
 logger.debug("Area of Triangle : {}",shapes.area(2.3,1.0));
-logger.debug("Area of Reactangle : {}",shapes.area(4,6));
-logger.info("...........................");
+logger.debug("Area of Rectangle : {}",shapes.area(2,3.2));
+logger.info(".........................................");
 
+logger.info("Runtime Polymorphism");
         Salary salary=new Salary("Jikar","UK",3600.00);
         Employee employee=new Salary("Asif","India",2400.00);
         logger.debug("Call mailCheck using Salary reference --");
-        logger.debug(salary.mailCheck());
+        String mail=salary.mailCheck();
+        logger.debug(mail);
         logger.debug("Call mailCheck using Employee reference--");
-        logger.debug(employee.mailCheck());
+        String mail1=employee.mailCheck();
+        logger.debug(mail1);
 
 
     }
